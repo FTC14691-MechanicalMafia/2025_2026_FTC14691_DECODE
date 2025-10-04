3fd5bafa185497b77e5574da820cd224335a3e32
Brad Sneade <bsneade@gmail.com>
2025-10-03 20:16:48 -0400

    Adding teleop mode and TODOs for the team

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleOpMode extends OpMode {
+
+
+    @Override
+    public void init() {
+        // TODO - Init our drive motors (set 0 power behavior, direction)
+        // TODO - Init our pinpoint driver / dead wheels
+        // TODO - April tag stuff (camera)
+        // TODO - init color identification
+        // TODO - init telemetry (display on the driver hub)
+        // TODO - Init intake
+        // TODO - init outtake
+        // TODO - init distance sensors
+        // TODO - init indicator light
+    }
+
+    @Override
+    public void start() {
+        super.start();
+
+        // TODO - look for april tags
+        // TODO - establish our coordinates / location from the april tag
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+
+        // TODO - stop all motors
+        // TODO - any final telemetry
+    }
+
+    @Override
+    public void loop() {
+        // Controller 1
+        // TODO - RT for boost
+        // TODO - LT for slow
+        // TODO - implement left stick for mechanic forward/strafe
+        // TODO - implement right stick for rotation
+
+        // Controller 2
+        // TODO - Left stick aiming for distance (motor speed)
+        // TODO - right stick aiming for distance angle
+        // TODO - X for auto aiming (overrides driver)
+        // TODO - L/RT for shoot ball
+        // TODO - B for intake on/off
+        // TODO - A for outtake on/off
+
+        // TODO - update telemetry
+    }
+}

72ffac4fb5fa72788c3c33e82c45fe7e4b027e50
arjav_p <arjav.m.prabhu@gmail.com>
2025-10-10 19:08:53 -0400

    added @teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.List;
-
+@TeleOp
 public class TeleOpMode extends OpMode {
 
     private ElapsedTime runtime = new ElapsedTime();

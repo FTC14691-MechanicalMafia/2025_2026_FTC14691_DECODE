2d3e288e4da86e90249087ae70debe38d22fdb8d
c00l-crypto-91 <colist2121@gmail.com>
2026-01-11 12:34:00 -0500

    updated advanceShoot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/AprilTagDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/AprilTagDriver.java
@@ -15,6 +15,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -59,6 +60,11 @@ public class AprilTagDriver {
         visionPortal = builder.build();
     }
     public List<AprilTag> detectAprilTags() {
+        if (processor == null) {
+            // added during competition to make auto op modes work again
+            return Collections.EMPTY_LIST;
+        }
+
         List<AprilTagDetection> currentDetections = processor.getDetections();
         telemetry.addData("Number AprilTags Detected", currentDetections.size());
 
